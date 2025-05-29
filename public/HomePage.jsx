c8f4032323c8cf7e1ee3b19826be4b4955f078e0
aaryan13084 <aaryankumar13084@gmail.com>
2025-05-29 12:34:38 +0530

    Initial commit

--- public/HomePage.jsx
+++ public/HomePage.jsx
@@ -69,7 +69,7 @@ const HomePage = () => {
   };
 
   return (
-    <div className="flex flex-col mt-10 h-screen bg-gray-900">
+    <div className="flex flex-col pt-10 h-[90vh] bg-gray-900">
       {/* Header Section */}
       <Header />
 
