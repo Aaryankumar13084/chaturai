31bc4bc6689793f4e48f3c8c340b753d1967bbd5
Aaryankumar13084 <aaryankumar13084@gmail.com>
2025-05-30 19:18:50 +0530

    Update Header.jsx

--- public/comp/Header.jsx
+++ public/comp/Header.jsx
@@ -1,53 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const Header = () => {
-  const [showMsg, setShowMsg] = useState(false);
+  return (<>
+  <div className='flex fixed top-0 right-0 left-0 text-white items-center place-content-between w-full bg-gray-800 border-t border-gray-700 '>
+  <div>
+      <img className='w-[60px]' src="https://i.ibb.co/pDFdf2y/icons8-menu-64.png" />
+  </div>
 
-  // Click handler jo message toggle karega
-  const handleClick = () => {
-    setShowMsg(true);
-
-    // 3 second ke baad message chhup jaye
-    setTimeout(() => setShowMsg(false), 3000);
-  };
-
-  return (
-    <>
-     {/* Left side message */}
-        <div className='w-[150px]'>
-          {showMsg && (
-            <div className='bg-green-600 text-white px-3 py-1 rounded flex fixed top-0 right-0 left-0'>
-              Coming Soon
-            </div>
-          )}
-        </div>
-
-      <div className='flex fixed top-0 right-0 left-0 text-white items-center justify-between w-full bg-gray-800 border-t border-gray-700 p-2'>
-
-   
-
-        {/* Image */}
-        <div>
-          <img
-            onClick={handleClick}
-            className='w-[60px] cursor-pointer'
-            src="https://i.ibb.co/pDFdf2y/icons8-menu-64.png"
-            alt="menu icon"
-          />
-        </div>
-
-        {/* Title and New Chat */}
-        <div className='flex items-center h-full w-[60%]'>
-          <h1 className="text-2xl">ChaturAi</h1>
-          <h1
-            onClick={handleClick}
-            className='text-xl bg-gray-700 px-2 py-px rounded mx-2 cursor-pointer'
-          >
-            New Chat
-          </h1>
-        </div>
-      </div>
-    </>
+  <div className='flex items-center place-content-between h-full w-[60%]'>
+    <h1 className=" text-2xl"> ChaturAi </h1>
+  <h1 className='text-xl bg-gray-700 px-2 py-px rounded mx-2'>New Chat</h1>
+ </div>
+  </div>
+</>
   )
 }
 
