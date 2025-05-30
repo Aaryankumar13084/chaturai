ada3870e2914e041c06ada66de95af3d1aa203fa
Aaryankumar13084 <aaryankumar13084@gmail.com>
2025-05-30 19:22:52 +0530

    Update Header.jsx

--- public/comp/Header.jsx
+++ public/comp/Header.jsx
@@ -1,19 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Header = () => {
-  return (<>
-  <div className='flex fixed top-0 right-0 left-0 text-white items-center place-content-between w-full bg-gray-800 border-t border-gray-700 '>
-  <div>
-      <img className='w-[60px]' src="https://i.ibb.co/pDFdf2y/icons8-menu-64.png" />
-  </div>
+  const [showMsg, setShowMsg] = useState(false);
 
-  <div className='flex items-center place-content-between h-full w-[60%]'>
-    <h1 className=" text-2xl"> ChaturAi </h1>
-  <h1 className='text-xl bg-gray-700 px-2 py-px rounded mx-2'>New Chat</h1>
- </div>
-  </div>
-</>
-  )
-}
+  const handleClick = () => {
+    setShowMsg(true);
+  };
 
-export default Header
+  // Automatically hide message after 3 seconds
+  useEffect(() => {
+    if (showMsg) {
+      const timer = setTimeout(() => setShowMsg(false), 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [showMsg]);
+
+  return (
+    <div className="relative">
+      {/* Header */}
+      <div className="flex fixed top-0 right-0 left-0 text-white items-center justify-between w-full bg-gray-800 border-t border-gray-700 p-3 z-50 h-[60px]">
+
+        {/* Menu icon */}
+        <div>
+          <img
+            onClick={handleClick}
+            className="w-[60px] cursor-pointer"
+            src="https://i.ibb.co/pDFdf2y/icons8-menu-64.png"
+            alt="menu icon"
+          />
+        </div>
+
+        {/* Title and New Chat */}
+        <div className="flex items-center h-full w-[60%]">
+          <h1 className="text-2xl">ChaturAi</h1>
+          <h1
+            onClick={handleClick}
+            className="text-xl bg-gray-700 px-2 py-px rounded mx-2 cursor-pointer"
+          >
+            New Chat
+          </h1>
+        </div>
+      </div>
+
+      {/* Notification overlay */}
+      <div
+        className={`
+          fixed top-3 left-3 bg-green-600 text-white px-4 py-2 rounded shadow-lg 
+          transform transition-all duration-300 ease-in-out
+          ${showMsg ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-10 pointer-events-none'}
+          z-60
+        `}
+      >
+        Coming Soon
+      </div>
+    </div>
+  );
+};
+
+export default Header;
